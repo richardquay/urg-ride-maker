2d2c042b9abb00a1a8d232ab840f00cfdde321e0
Richard Quay <richardquay@gmail.com>
2025-06-05 08:29:07 -0500

    refactor: rename settings command to urg-settings

--- src/commands/slash/settings.ts
+++ src/commands/slash/settings.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const data = new SlashCommandBuilder()
-    .setName('settings')
+    .setName('urg-settings')
     .setDescription('Configure bot settings')
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addSubcommand(subcommand =>
